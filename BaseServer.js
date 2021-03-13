6f3059ca4acba38530b121b530f8c6d8619ed630
maxert <max97-12@ukr.net>
2021-03-13 15:08:02 +0200

    heroku update

--- BaseServer.js
+++ BaseServer.js
@@ -1,11 +1,12 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs')
-
+let counter = 0;
 module.exports = {
     start: function () {
         const scrape = async () => {
             const browser = await puppeteer.launch({headless: true});
             const page = await browser.newPage();
+            const pageTwo = await browser.newPage();
             await page.goto('http://services.hneu.edu.ua:8081/schedule/selection.jsf');
 
 
@@ -72,11 +73,55 @@ module.exports = {
                                     let elements = document.querySelectorAll(`select#group-form\\:student option`); // Выбираем все товары
                                     elements.forEach(element => {
                                         data.push({value: element.value, name: element.innerHTML})
+
+
                                     })
                                     return data;
                                 })
+
+
                                 student.student = click;
-                                console.log(student)
+                                for (const studentEl of student.student) {
+                                    if (studentEl.value === " ") {
+                                        await pageTwo.goto(`http://services.hneu.edu.ua:8081/schedule/schedule?group=${student.value}&week=29`)
+
+                                        const result = await pageTwo.evaluate(() => {
+                                            let data = [];
+                                            let elements = document.querySelectorAll('body>table>tbody>tr');
+                                            elements.forEach(element => {
+                                                element.querySelectorAll("#cell").forEach((Number,i)=>{
+                                                    if(Number.querySelectorAll("#element-table")[0]){
+                                                        data.push({PARA:parseInt(Number.parentElement.querySelectorAll(".pair")[0].innerText),numberDay:i,Block:Number.innerHTML})
+
+                                                    }
+                                                });
+                                            })
+
+                                            return data;
+                                        });
+                                        studentEl.schedule = result;
+                                    } else {
+                                        await pageTwo.goto(`http://services.hneu.edu.ua:8081/schedule/schedule?group=${student.value}&week=29&student=${studentEl.value}`)
+                                        const result = await pageTwo.evaluate(() => {
+                                            let data = [];
+                                            let elements = document.querySelectorAll('body>table>tbody>tr');
+                                            elements.forEach(element => {
+                                                element.querySelectorAll("#cell").forEach((Number,i)=>{
+                                                    if(Number.querySelectorAll("#element-table")[0]){
+                                                        data.push({PARA:parseInt(Number.parentElement.querySelectorAll(".pair")[0].innerText),numberDay:i,Block:Number.innerHTML})
+
+                                                    }
+                                                });
+                                            })
+
+                                            return data;
+                                        });
+                                        studentEl.schedule = result;
+                                    }
+
+
+                                }
+
                             }
 
                         }
@@ -88,50 +133,52 @@ module.exports = {
                 }
 
             }
-            await page.click('#teacher-tab_cell')
-            const resultTeacher = await page.evaluate(() => {
-                let data = [];
-                let elements = document.querySelectorAll('select.select');
-
-                for (const element of elements) {
-                    let valueName = [];
-                    let title = element.id;
-                    let elemVal = element.querySelectorAll('option');
-                    elemVal.forEach(element => {
-                        valueName.push({value: element.value, name_schedule: element.innerHTML})
-                    })
-                    data.push({title, valueName});
-                    for (const element of elements) {
-                        let valueName = [];
-                        let title = element.id;
-                        let elemVal = element.querySelectorAll('option');
-                        elemVal.forEach(element => {
-                            valueName.push({value: element.value, name_schedule: element.innerHTML})
-                        })
-                        data.push({title, valueName});
-                    }
-                }
-                return data;
-            });
-
-            for (const resultTeacherList of resultTeacher[6].values) {
-                await page.select("#teacher-form\\:department", resultTeacherList.value);
-                await page.waitFor(50);
-                const click = await page.evaluate(() => {
-                    let data = [];
-                    let elements = document.querySelectorAll(`select#teacher-form\\:employee option`); // Выбираем все товары
-                    elements.forEach(element => {
-                        data.push({value: element.value, name: element.innerHTML})
-                    })
-                    return data;
-                })
-                resultTeacherList.name_teacher = click;
-            }
+            // await page.click('#teacher-tab_cell')
+            // const resultTeacher = await page.evaluate(() => {
+            //     let data = [];
+            //     let elements = document.querySelectorAll('select.select');
+            //
+            //     for (const element of elements) {
+            //         let valueName = [];
+            //         let title = element.id;
+            //         let elemVal = element.querySelectorAll('option');
+            //         elemVal.forEach(element => {
+            //             valueName.push({value: element.value, name_schedule: element.innerHTML})
+            //         })
+            //         data.push({title, valueName});
+            //         for (const element of elements) {
+            //             let valueName = [];
+            //             let title = element.id;
+            //             let elemVal = element.querySelectorAll('option');
+            //             elemVal.forEach(element => {
+            //                 console.log({value: element.value, name: element.innerHTML})
+            //                 valueName.push({value: element.value, name_schedule: element.innerHTML})
+            //             })
+            //             data.push({title, valueName});
+            //         }
+            //     }
+            //     return data;
+            // });
+            //
+            // for (const resultTeacherList of resultTeacher[6].values) {
+            //     await page.select("#teacher-form\\:department", resultTeacherList.value);
+            //     await page.waitFor(50);
+            //     const click = await page.evaluate(() => {
+            //         let data = [];
+            //         let elements = document.querySelectorAll(`select#teacher-form\\:employee option`); // Выбираем все товары
+            //         elements.forEach(element => {
+            //             data.push({value: element.value, name: element.innerHTML})
+            //         })
+            //
+            //         return data;
+            //
+            //     })
+            //     resultTeacherList.name_teacher = click;
+            // }
 
             browser.close();
             return {
                 group_student: result,
-                teacher: resultTeacher[6]
             }
         }
         scrape().then((value) => {
